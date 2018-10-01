2c95ef60e3d2df205b5a69364997f2fa918fe998
IvannMG <ivannm@theodo.fr>
2018-10-01 21:03:28 +0200

    Try to implement GMaps

--- src/start-polymer3.js
+++ src/start-polymer3.js
@@ -10,82 +10,23 @@
 
 // Import statements in Polymer 3.0 can now use package names.
 // polymer-element.js now exports PolymerElement instead of Element,
-// so no need to change the symbol. 
+// so no need to change the symbol.
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '@polymer/polymer/lib/elements/dom-if.js';
-import '@polymer/paper-checkbox/paper-checkbox.js';
+import '../node_modules/@em-polymer/google-map/google-map.js';
 import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings';
 
 class StartPolymer3 extends PolymerElement {
-  static get properties () {
-    return {
-      message: {
-        type: String,
-        value: ''
-      },
-      pie: {
-        type: Boolean,
-        value: false,
-        observer: 'togglePie'
-      },
-      loadComplete: {
-        type: Boolean,
-        value: false
-      }
-    };
-  }
-
-  constructor() {
-    // If you override the constructor, always call the 
-    // superconstructor first.
-    super();
-    // Resolve warning about scroll performance 
-    // See https://developers.google.com/web/updates/2016/06/passive-event-listeners
-    setPassiveTouchGestures(true);
-    this.message = 'Hello World! I\'m a Polymer element :)';
-  }
-
-  ready(){
-    // If you override ready, always call super.ready() first.
+  ready () {
     super.ready();
-    // Output the custom element's HTML tag to the browser console.
-    // Open your browser's developer tools to view the output.
-    console.log(this.tagName);
-    this.$.omgpie.focus();
+    debugger
   }
-  
-  togglePie(){
-    if(this.pie && !this.loadComplete) {
-      // See https://developers.google.com/web/updates/2017/11/dynamic-import
-      import('./lazy-element.js').then((LazyElement) => {
-        console.log("LazyElement loaded");
-      }).catch((reason) => {
-        console.log("LazyElement failed to load", reason);
-      });
-      this.loadComplete = true;
-    }
-  }
-
   static get template () {
     // Template getter must return an instance of HTMLTemplateElement.
     // The html helper function makes this easy.
     return html`
-      <style>
-        paper-checkbox {
-          --paper-checkbox-checked-ink-color: #FFFFFF;
-          --paper-checkbox-unchecked-ink-color: #FFFFFF;
-        }
-      </style>
-
-      <h1>Start Polymer 3.0</h1>
-      <p>[[message]]</p>
-      <paper-checkbox id="omgpie"
-        toggles
-        noink
-        checked={{pie}}>I like pie.</paper-checkbox>
-      <template is="dom-if" if=[[pie]]>
-        <lazy-element><p>lazy loading...</p></lazy-element>
-      </template>
+      <google-map fit-to-markers api-key="AIzaSyD3E1D9b-Z7ekrT3tbhl_dy8DCXuIuDDRc">
+        <google-map-marker latitude="37.78" longitude="-122.4" draggable="true"></google-map-marker>
+      </google-map>
     `;
   }
 }
